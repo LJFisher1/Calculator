cb081b3be364e25b74d3aa3e0a019f1a73fb98a1
LJFisher1 <ljfisher1@student.fullsail.edu>
2022-11-02 15:24:20 -0500

    Added click event logic

--- SWE_App/Window.h
+++ SWE_App/Window.h
@@ -29,5 +29,30 @@ private:
 	wxButton* equal = nullptr;
 public:
 	Window();
+
+	void OnButtonClicked0(wxCommandEvent& evt);
+	void OnButtonClicked1(wxCommandEvent& evt);
+	void OnButtonClicked2(wxCommandEvent& evt);
+	void OnButtonClicked3(wxCommandEvent& evt);
+	void OnButtonClicked4(wxCommandEvent& evt);
+	void OnButtonClicked5(wxCommandEvent& evt);
+	void OnButtonClicked6(wxCommandEvent& evt);
+	void OnButtonClicked7(wxCommandEvent& evt);
+	void OnButtonClicked8(wxCommandEvent& evt);
+	void OnButtonClicked9(wxCommandEvent& evt);
+	void OnButtonClicked10(wxCommandEvent& evt);
+	void OnButtonClicked11(wxCommandEvent& evt);
+	void OnButtonClicked12(wxCommandEvent& evt);
+	void OnButtonClicked13(wxCommandEvent& evt);
+	void OnButtonClicked14(wxCommandEvent& evt);
+	void OnButtonClicked15(wxCommandEvent& evt);
+	void OnButtonClicked16(wxCommandEvent& evt);
+	void OnButtonClicked17(wxCommandEvent& evt);
+	void OnButtonClicked18(wxCommandEvent& evt);
+	void OnButtonClicked19(wxCommandEvent& evt);
+	void OnButtonClicked20(wxCommandEvent& evt);
+	void OnButtonClicked21(wxCommandEvent& evt);
+
+	wxDECLARE_EVENT_TABLE();
 };
 
